cb3721bb038c30cc743f89003f6a61532a6dcda8
mignonnesaurus <elifloresch@gmail.com>
2018-02-05 21:37:58 +0100

    moving tests to their own folder

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount, render, configure } from 'enzyme';
+import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import App from '../src/App';
 
 configure({ adapter: new Adapter() });
 
