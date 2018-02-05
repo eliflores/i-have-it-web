d846423f282b1d9e889c9502ee6c2bd8ce4f1be0
Ely Flores <elizabeth.flores@marleyspoon.com>
2018-02-05 18:20:28 +0100

    setup tests for the app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount, render, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+configure({ adapter: new Adapter() });
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const wrapper = shallow(<App/>);
+  console.log(wrapper);
 });
